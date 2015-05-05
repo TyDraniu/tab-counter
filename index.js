aa7a3a0880d70cf12ff6918fb6347c5ba8154a37
Krzysztof Głębowicz <krzysztof.glebowicz@it-pkp.pl>
2015-05-05 11:43:22 +0200

    Doesn't work yet

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 'use strict';
 
 let { Cc, Ci } = require('chrome');
-
 var self = require('sdk/self');
+var { viewFor } = require("sdk/view/core");
+var window = viewFor(require("sdk/windows").browserWindows[0]);
 
-var btn = require('sdk/ui/button')
 
-var w = Cc['@mozilla.org/appshell/window-mediator;1'].getService(Ci.nsIWindowMediator).getEnumerator('navigator:browser');
+function TabsNumber() {
+	var w = Cc['@mozilla.org/appshell/window-mediator;1'].getService(Ci.nsIWindowMediator).getEnumerator('navigator:browser');
+	var t = 0;
+	while (w.hasMoreElements())
+		t += w.getNext().document.getElementById('content').mTabs.length;
+	return t;
+}
 
-var newtab = document.getElementById('new-tab-button');
-var t = 0;
 
-while (w.hasMoreElements())
-t += w.getNext().document.getElementById('content').mTabs.length;
-if (t == 1) {
-    newtab.setAttribute('tooltiptext', 'You have 1 tab');
-} 
-else {
-    newtab.setAttribute('tooltiptext', 'You have ' + t + ' tabs');
+var newtab = window.document.getElementById('new-tab-button');
+
+
+if (newtab != null) {
+	var tn = TabsNumber();
+	if (tn == 1) {
+		newtab.setAttribute('tooltiptext', 'You have 1 tab');
+	} 
+	else {
+		newtab.setAttribute('tooltiptext', 'You have ' + tn + ' tabs');
+	}
+
 }
